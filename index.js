7d567a72b164ef790c24470c3c88fa0410eea1ac
Yonatan Kawo <yonas478@gmail.com>
2021-07-30 18:48:15 -0500

    commit tree b6744bf8cfd3df8e37983be57f6c1668d31c1ec8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+  
+function superbowlWin(record) {
+  let winnerYear = record.find((record) => record.result === "W");
+  if (winnerYear){
+      return winnerYear.year;
+    }
+    else{
+        return undefined;
+    } 
+}
 
+console.log(superbowlWin(record))
